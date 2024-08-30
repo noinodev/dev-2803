aa4bec8defed950bb6a221326b3ac07f1d54b7f6
noino <nathanmburg@gmail.com>
2024-08-30 23:36:39 +1000

    git vim cygwin init

---
+++ dev-2803/commands.c
@@ -0,0 +1,4 @@
+char** get_tokens(char* cmd){
+	
+}
+void cmd_calculate(char** tokens);
---
+++ dev-2803/commands.h
@@ -0,0 +1,8 @@
+char** get_tokens(char* cmd);
+void cmd_calculate(char** tokens);
+void cmd_time(char** tokens);
+void cmd_path(char** tokens);
+void cmd_sys(char** tokens);
+void cmd_put(char** tokens);
+void cmd_get(char** tokens);
+void cmd_quit(char** tokens);
